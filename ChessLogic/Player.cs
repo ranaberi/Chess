0729199eb58900339d20d2144fa7dec6b5df4e8e
Rana <ranaberi25@gmail.com>
2024-02-17 21:11:57 +0100

    player enum, direction and poistion classes created

---
+++ ChessLogic/Player.cs
@@ -0,0 +1,33 @@
+﻿namespace ChessLogic
+{
+    /// <summary>
+    /// The player enum is used to store which player turn it is and who won the game.
+    /// It is also used to represent the color of the chess pieces.
+    /// </summary>
+    public enum Player
+    {
+        //None is used to represent the game state when no player has won (in case of a draw).
+        None,
+        White,
+        Black
+    }
+
+    
+    public static class PlayerExtensions 
+    {
+        /// <summary>
+        /// Finds the opponent of a given player.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns>the player's opponent</returns>
+        public static Player Opponent (this Player player)
+        {
+            return player switch
+            {
+                Player.Black => Player.White,
+                Player.White => Player.Black,
+                _ => Player.None,
+            };
+        }
+    }
+}
