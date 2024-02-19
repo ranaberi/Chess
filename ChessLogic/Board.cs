e8b4e5c91598e2e0cc31dcbc335c9bcb69c9f894
Rana <ranaberi25@gmail.com>
2024-02-19 21:12:48 +0100

    indexers

---
+++ ChessLogic/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    public class Board
+    {
+        private readonly Piece[,] pieces = new Piece[8, 8];
+        
+        private Piece this[int row, int col]
+        {
+            get { return pieces[row, col]; }
+            set { pieces[row, col] = value; }
+        }
+
+        public Piece this[Position pos]
+        {
+            get { return this[pos.Row, pos.Column]; }
+            set { this[pos.Row, pos.Column] = value; }
+        }
+    }
+}
