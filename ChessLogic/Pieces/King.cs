e26c19ccac0976c0cba39061442e70bcd12ae7ba
Rana <ranaberi25@gmail.com>
2024-02-20 13:18:50 +0100

    King moves

--- ChessLogic/Pieces/King.cs
+++ ChessLogic/Pieces/King.cs
@@ -5,6 +5,18 @@
         public override PieceType Type => PieceType.King;
         public override Player Color { get; }
 
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.North,
+            Direction.South,
+            Direction.East,
+            Direction.West,
+            Direction.NorthEast,
+            Direction.NorthWest,
+            Direction.SouthEast,
+            Direction.SouthWest
+        };
+
         public King(Player color)
         {
             Color = color;
@@ -14,6 +26,30 @@
             King copy = new King(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        private IEnumerable<Position> MovePositions(Position from, Board board)
+        {
+            foreach (Direction dir in dirs)
+            {
+                Position to = from + dir;
+                if(!Board.IsInside(to))
+                {
+                    continue;
+                }
+                if(board.IsEmpty(to) || board[to].Color != Color)
+                {
+                    yield return to;
+                }
+            }
+        }
+
+        public override IEnumerable<Move> GetMoves(Position from, Board board)
+        {
+            foreach(Position to in MovePositions(from, board))
+            {
+                yield return new NormalMove(from, to);
+            }
         }
     }
 }
