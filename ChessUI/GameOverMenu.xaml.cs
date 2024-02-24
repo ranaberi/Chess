674c2b2ad2f4544803610b466beee14b2c5d9f6c
Rana <ranaberi25@gmail.com>
2024-02-24 11:33:43 +0100

    game over menu implementation

--- ChessUI/GameOverMenu.xaml.cs
+++ ChessUI/GameOverMenu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,19 +21,58 @@ namespace ChessUI
     /// </summary>
     public partial class GameOverMenu : UserControl
     {
-        public GameOverMenu()
+        public event Action<Option> OptionSelected;
+        public GameOverMenu(GameState gameState)
         {
             InitializeComponent();
+            Result result = gameState.Result;
+            WinnerText.Text = GetWinnerText(result.Winner);
+            ReasonText.Text = GetReasonText(result.Reason, gameState.CurrentPlayer);
+
+        }
+
+        private static string GetWinnerText(Player winner)
+        {
+            return winner switch
+            {
+                Player.White => "WHITE WINS!",
+                Player.Black => "BLACK WINS!",
+                _ => "IT'S A DRAW!",
+            };
+        }
+
+        private static string PlayerString(Player player)
+        {
+            return player switch
+            {
+                Player.White => "WHITE",
+                Player.Black => "BLACK",
+                _ => "",
+            };
+        }
+
+        private string GetReasonText(EndReason reason, Player currentPlayer)
+        {
+            return reason switch
+            {
+                EndReason.Checkmate => $"CHECKMATE - {PlayerString(currentPlayer)} CAN'T MOVE",
+                EndReason.Stalemate => $"STALEMATE - {PlayerString(currentPlayer)} CAN'T MOVE",
+                EndReason.InsufficientMaterial => "DRAW - INSUFFICIENT MATERIAL",
+                EndReason.ThreefoldRepetition => "DRAW - THREEFOLD REPETITION",
+                EndReason.FiftyMoveRule => "DRAW - FIFTY MOVE RULE",
+                _ => ""
+            };
         }
 
         private void Restart_click(object sender, RoutedEventArgs e)
         {
+            OptionSelected?.Invoke(Option.Restart);
 
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
-
+            OptionSelected?.Invoke(Option.Exit);
         }
     }
 }
--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -66,6 +66,11 @@ namespace ChessUI
 
         private void BoardGrid_MouseDown(object sender, MouseButtonEventArgs e)
         {
+
+            if (IsMenuOnScreen())
+            {
+                return;
+            }
             Point point = e.GetPosition(BoardGrid);
             Position pos = ToSquarePosition(point);
 
@@ -114,6 +119,11 @@ namespace ChessUI
             gameState.MakeMove(move);
             DrawBoard(gameState.Board);
             SetCursor(gameState.CurrentPlayer);
+
+            if(gameState.IsGameOver())
+            {
+                ShowGameOver();
+            }
         }
         private void CacheMove(IEnumerable<Move> moves)
         {
@@ -154,6 +164,36 @@ namespace ChessUI
             }
         }
 
+        private bool IsMenuOnScreen()
+        {
+            return MenuContainer.Content != null;
+        }
+        private void ShowGameOver()
+        {
+            GameOverMenu gameOverMenu = new GameOverMenu(gameState);
+            MenuContainer.Content = gameOverMenu;
+            gameOverMenu.OptionSelected += option =>
+            {
+                if (option == Option.Restart)
+                {
+                    MenuContainer.Content = null;
+                    RestartGame();
+                }
+                else
+                {
+                    Application.Current.Shutdown();
+                }
+            };
+        }
+
+        private void RestartGame()
+        {
+            HideHighlights();
+            moveCache.Clear();
+            gameState = new GameState(Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
+            SetCursor(gameState.CurrentPlayer);
+        }
         
     }
 }
